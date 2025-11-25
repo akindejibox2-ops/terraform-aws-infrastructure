0d43f42f7edb2f35f4ad4fc25382c83d3e9a07cc
akindejibox2-ops <akindejibox2@gmail.com>
2025-11-25 18:09:28 +0000

    Create Main.tf

---
+++ Main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr = var.vpc_cidr
+}
